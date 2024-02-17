ff4f950f9ba406c05f974ad6e8c7e4258a044859
Артём Никифоров <152002611+dom2411edinstvo@users.noreply.github.com>
2024-02-18 00:10:47 +0500

    init

---
+++ index.js
@@ -0,0 +1,3 @@
+const button = document.querySelector('button');
+const output = document.querySelector('.output');
+button.addEventListener('click', function() { output.innerHTML = 'Мама, я тебя люблю!';});
